49a2eeddd5ae9c465977298ab571f05aedaec0a5
Ian Tanui <72918706+iantanui@users.noreply.github.com>
2022-04-14 01:36:28 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,56 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects correct function to perform based on user input
- * @s: operator passed as argument
- * Return: idk yet
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
