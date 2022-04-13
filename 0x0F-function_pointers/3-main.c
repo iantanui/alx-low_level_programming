5196414be1cc6ba695be7f3c47a2e817471b0c3e
Ian Tanui <72918706+iantanui@users.noreply.github.com>
2022-04-14 00:32:16 +0300

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	return (0);
+}
