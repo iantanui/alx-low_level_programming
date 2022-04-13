c645655f177a5911f59e4d6a5d407cc9cb248125
Ian Tanui <72918706+iantanui@users.noreply.github.com>
2022-04-14 01:19:07 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,24 +2,39 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcodes
- * @argc: number of arguments passed 
- * @argv: array of arguments
- * Return: 0 if successful
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	if (atoi(argv[1]) < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	return (0);
+int i, n;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+exit(2);
+}
+for (i = 0; i < n; i++)
+{
+printf("%02hhx", *((char *)main + i));
+if (i < n - 1)
+{
+printf(" ");
+}
+else
+{
+printf("\n");
+}
+}
+return (0);
 }
