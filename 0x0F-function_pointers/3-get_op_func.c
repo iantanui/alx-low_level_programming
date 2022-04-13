35ad60777f7b31b4a7c969f0be30560bdcbd9cb3
Ian Tanui <72918706+iantanui@users.noreply.github.com>
2022-04-14 00:29:15 +0300

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,10 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects correct function to perform based on user input
+ * @s: operator passed as argument
+ * Return: idk yet
+ */
+int (*get_op_func(char *s))(int, int)
+{
+}
