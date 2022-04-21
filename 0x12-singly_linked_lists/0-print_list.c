5489b128e4fb1f6c3ecb58f52f048ff1591936f2
iantanui <rutooian@gmail.com>
2022-04-21 10:19:29 -0700

    first

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/*
+ * print_list - prints linked list
+ * @h: list to print
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i, n = 0, count = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)");
+		return;
+	}
+
+	for (i = 0; h->next; i++)
+	{
+		count++;
+	}
+	return (count);
+}
